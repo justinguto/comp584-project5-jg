a7111b15a702d70e59f6fba617c2f735594d1367
justinguto <163184050+justinguto@users.noreply.github.com>
2025-12-12 14:29:49 -0800

    Create helloworld.js

---
+++ helloworld.js
@@ -0,0 +1,14 @@
+
+---
+
+## 01-hello-world.js
+
+```js
+/**
+ * SOURCES:
+ * - learnyounode workshopper instructions (HELLO WORLD) :contentReference[oaicite:2]{index=2}
+ *
+ * Goal: print "HELLO WORLD" to stdout.
+ */
+
+console.log('HELLO WORLD');
